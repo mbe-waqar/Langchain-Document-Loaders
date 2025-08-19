ffe10c0d7b61300a44e3789f2b80b5a2226645e0
Waqar Ali <waqar0343930@gmail.com>
2025-08-19 14:08:32 +0500

    Directoty loader

---
+++ directory_loader.py
@@ -0,0 +1,11 @@
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+
+loader = DirectoryLoader(
+    path="books",
+    glob="*.pdf",
+    loader_cls=PyPDFLoader
+)
+
+documents = loader.load()
+
+print(len(documents))
